40e5b30992f4182ff7b39496b85937368ab706ec
dmitriy <dinvest4@gmail.com>
2022-12-24 21:07:54 +0300

    Написана программа, решающая поставленную задачу

---
+++ programm/Program.cs
@@ -0,0 +1,37 @@
+﻿
+string[] arrayStart = new string[] { "hello", "2", "world", ":-)"};
+
+
+string [] checkArrayElement (string [] arrStart)
+{
+    int j = 0;
+    int count = 0;
+    for (int i = 0; i < arrayStart.Length; i++)
+{
+    if (arrStart[i].Length <= 3){
+        j++;
+    }
+}
+string[] arrayFinal = new string[j];
+
+for (int i = 0; i < arrayStart.Length; i++)
+{
+    if (arrStart[i].Length <= 3){
+    arrayFinal [count] = arrStart[i];
+    count++;
+}
+
+}return arrayFinal;
+}
+
+void printArray (string [] arr){
+for (int i = 0; i < arr.Length; i++)
+{
+    Console.Write($"{arr[i]} ");
+}
+}
+
+printArray(checkArrayElement(arrayStart));
+
+
+
